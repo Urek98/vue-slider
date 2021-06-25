4786430800d8411f9617e6e3951925d0b57574a3
Urek98 <gianluca.argo1998@libero.it>
2021-06-25 15:15:45 +0200

    create funzioni slider

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,8 +1,37 @@
 Vue.config.devtools = true;
 
-const app = new Vue (
+const app = new Vue(
     {
         el: '#app',
+        data: {
+            imgList: [
+                './img/background-1.jpg',
+                './img/background-2.jpg',
+                './img/background-3.jpg',
+                './img/background-4.jpg'
+            ],
+            imgIndex: 0,
+        },
+        methods: {
+            next: function () {
+
+                if (this.imgIndex === (this.imgList.length - 1)) {
+                    this.imgIndex = 0;
+                } else {
+                    this.imgIndex++;
+                }
+            },
+
+            prev: function () {
+
+                if(this.imgIndex === 0){
+                    this.imgIndex = this.imgList.length -1;
+                } else {
+                    this.imgIndex--;
+
+                }
+            },
+        }
     }
 
 
